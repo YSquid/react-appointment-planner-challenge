7acf9a4fe2c977d9378edcfb45990a53623504b3
Ysquid <ahk648@gmail.com>
2022-10-18 15:48:39 -0600

    Appointment Form done except for contact picker

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ContactPicker} from './../contactPicker/ContactPicker'
 
 export const AppointmentForm = ({
   contacts,
@@ -10,7 +11,7 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
   const getTodayString = () => {
     const [month, day, year] = new Date()
@@ -20,6 +21,25 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+      </label>
+
+      <label>
+        Date:
+        <input type='date' name='date' min={getTodayString()} value={date} onChange={(e) => setDate(e.target.value)}></input>
+      </label>
+
+      <label>
+        Time:
+        <input type='time' name='time' value={time} onChange={(e) => setTime(e.target.value)}></input>
+      </label>
+
+      <ContactPicker contacts={contacts}/>
+
+      <input type="submit"></input>
+    </form>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -7,7 +7,7 @@ export const AppointmentsPage = (props) => {
   Define state variables for 
   appointment info
   */
- const [currentTitle, setCurrentTitle] = useState('');
+ const [title, setTitle] = useState('');
  const [contact, setContact] = useState('');
  const [date, setDate] = useState('');
  const [time, setTime] = useState('');
@@ -17,8 +17,8 @@ export const AppointmentsPage = (props) => {
     /*
     Add contact info and clear data  
     */
-   props.addAppointment(currentTitle, contact, date, time);
-   setCurrentTitle('');
+   props.addAppointment(title, contact, date, time);
+   setTitle('');
    setContact('');
    setDate('');
    setTime('');
@@ -29,8 +29,8 @@ export const AppointmentsPage = (props) => {
       <section>
         <h2>Add Appointment</h2>
         <AppointmentForm
-        currentTitle={currentTitle}
-        setCurrentTitle={setCurrentTitle}
+        title={title}
+        setTitle={setTitle}
         contact={contact}
         setContact={setContact}
         date={date}
@@ -38,6 +38,7 @@ export const AppointmentsPage = (props) => {
         time={time}
         setTime={setTime}
         handleSubmit={handleSubmit}
+        contacts={props.contacts}
         />
       </section>
       <hr />
