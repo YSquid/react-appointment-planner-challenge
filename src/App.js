15d3ab4bc3d49be2255b0590e4ab2069b70ffa55
Ysquid <ahk648@gmail.com>
2022-10-18 11:34:46 -0600

    App.js - state and event handlers done. Passed to children

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -10,6 +10,9 @@ function App() {
   contacts and appointments 
   */
 
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
@@ -19,6 +22,17 @@ function App() {
   Implement functions to add data to
   contacts and appointments
   */
+
+  const addContact = (name, phone, email) => {
+    setContacts([...contacts, { name: name, phone: phone, email: email }]);
+  };
+
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      { title: title, contact: contact, date: date, time: time },
+    ]);
+  };
 
   return (
     <>
@@ -36,12 +50,12 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            {/* Add props to ContactsPage */}
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
             {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage contacts={contacts} appointments={appointments} addAppointment={addAppointment} />
           </Route>
         </Switch>
       </main>
