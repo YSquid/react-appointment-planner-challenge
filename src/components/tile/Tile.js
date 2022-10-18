a92822afa29fda1ca4cc8104c43f31f2ea12b6f5
Ysquid <ahk648@gmail.com>
2022-10-18 13:56:36 -0600

    TileList map function

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export const Tile = () => {
   return (
     <div className="tile-container">
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,8 +1,13 @@
 import React from "react";
+import {Tile} from './../tile/Tile'
 
-export const TileList = () => {
+export const TileList = (props) => {
+  const mockArr = [{name: 'Albert', email: 'soup@soup.com', phone: '3064916395'}, {name: 'Maggie', email: 'salad@salad.com', phone: '3067897898'},{name: 'Maggie', email: 'salad@salad.com', phone: '3067897898'}]
   return (
     <div>
+      {mockArr.map((object, index) =>
+      <li><Tile object={object} key={index} /></li>
+      )}
       
     </div>
   );
