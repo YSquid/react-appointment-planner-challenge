527ea0ecbed9c9370d806cefeb984eae9d584236
Ysquid <ahk648@gmail.com>
2023-01-07 13:57:48 -0600

    added test data for ocntact and appoinments, initialized with it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
@@ -11,8 +11,22 @@ function App() {
   contacts and appointments 
   */
 
-  const [contacts, setContacts] = useState([]);
-  const [appointments, setAppointments] = useState([]);
+  const testContacts = [
+    { name: "Ahmad", phone: "306-153-7498", email: "ahmad1008@gmail.com" },
+    { name: "Sam", phone: "306-401-7981", email: "sam123@gmail.com" },
+    { name: "Emma Sampson", phone: "306-897-0549", email: "emmasews20@gmail.com" },
+    { name: "Equid", phone: "306-178-0058", email: "taxpros20@gmail.com" }
+  ];
+
+  const testAppointments = [
+    {title: 'Ahmad intake', contact: 'Ahmad', date: '2023-01-11', time: '12:45pm'},
+    {title: 'Sam renewal', contact: 'Sam', date: '2023-02-14', time: '7:30am'},
+    {title: 'Emma prospect', contact: 'Emma', date: '2023-01-21', time: '3:45pm'}, 
+    {title: 'Sam renewal', contact: 'Sam', date: '2023-02-14', time: '1:00pm'}
+  ]
+
+  const [contacts, setContacts] = useState(testContacts);
+  const [appointments, setAppointments] = useState(testAppointments);
 
   const ROUTES = {
     CONTACTS: "/contacts",
@@ -38,10 +52,18 @@ function App() {
   return (
     <>
       <nav>
-        <NavLink to={ROUTES.CONTACTS} activeClassName="active" className="navLink">
+        <NavLink
+          to={ROUTES.CONTACTS}
+          activeClassName="active"
+          className="navLink"
+        >
           <h2>Contacts</h2>
         </NavLink>
-        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active" className="navLink">
+        <NavLink
+          to={ROUTES.APPOINTMENTS}
+          activeClassName="active"
+          className="navLink"
+        >
           <h2>Appointments</h2>
         </NavLink>
       </nav>
